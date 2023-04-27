403f5c0c6504f00359b80001cb01617265c29893
Vlad G <b100dian@gmail.com>
2023-04-28 00:25:15 +0300

    Double-tap to wake

--- sparse/usr/bin/droid/droid-hal-early-init.sh
+++ sparse/usr/bin/droid/droid-hal-early-init.sh
@@ -11,3 +11,7 @@ ln -s /dev/bsg/ufs-bsg0 /dev/ufs-bsg
 
 # Allows libui_compat_layer to work
 mount -o bind /dev/null /vendor/lib64/hw/gralloc.default.so
+
+# D2TW
+echo 1 > /sys/devices/platform/soc/990000.i2c/i2c-1/1-0038/fts_aod_ctrl_mode
+echo 1 > /sys/devices/platform/soc/990000.i2c/i2c-1/1-0038/fts_gesture_mode

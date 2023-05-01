00dcaa3754109cf7da8a8f8958d13db71a6f4dc2
Vlad G <b100dian@gmail.com>
2023-05-01 17:55:18 +0300

    Fix droid-misc script execution + move some stuff in it

--- sparse/usr/bin/droid/droid-hal-early-init.sh
+++ sparse/usr/bin/droid/droid-hal-early-init.sh
@@ -11,14 +11,3 @@ ln -s /dev/bsg/ufs-bsg0 /dev/ufs-bsg
 
 # Allows libui_compat_layer to work
 mount -o bind /dev/null /vendor/lib64/hw/gralloc.default.so
-
-### Below commands are not technically related to droid-hal-init starting
-### they are just here for convenience.
-
-# D2TW
-echo 1 > /proc/driver/dclick
-
-# Power vibra
-/sbin/modprobe aw8697
-echo 1 > /sys/class/leds/vibrator/activate_mode
-
--- sparse/usr/bin/droid/droid-misc.sh
+++ sparse/usr/bin/droid/droid-misc.sh
@@ -1,4 +1,12 @@
 #!/bin/sh
-#Misc setup script
 
+# Waydroid
 ln -sf /dev/binderfs/* /dev/
+
+# D2TW
+echo 1 > /proc/driver/dclick
+
+# Power vibra
+/sbin/modprobe aw8697
+echo 1 > /sys/class/leds/vibrator/activate_mode
+

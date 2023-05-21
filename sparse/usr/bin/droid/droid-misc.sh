00d13fbffd42e635535beb9792acf4a7d0d4695a
Vlad G <b100dian@gmail.com>
2023-05-22 00:32:18 +0300

    fpd experiments

--- sparse/usr/bin/droid/droid-misc.sh
+++ sparse/usr/bin/droid/droid-misc.sh
@@ -12,3 +12,10 @@ echo 1 > /sys/class/leds/vibrator/activate_mode
 
 # Video recording
 setprop debug.stagefright.c2inputsurface -1
+
+# sailfish-fpd-community
+touch /odm/lib64/android.hardware.biometrics.fingerprint@2.1.so
+mount -o ro,bind /apex/com.android.vndk.v30/lib64/android.hardware.biometrics.fingerprint@2.1.so /odm/lib64/android.hardware.biometrics.fingerprint@2.1.so
+
+# Still qseecomd, fingerprint
+ln -s /dev/bsg/0\:0\:0\:49476  /dev/0\:0\:0\:49476
